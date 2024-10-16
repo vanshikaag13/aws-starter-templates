d00761fcaf53155846d4ced8966ad1b7d720af65
vanshikaag13 <agrawalvanshi286@gmail.com>
2024-10-17 01:37:56 +0530

    bug fixes 5.0

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -27,15 +27,6 @@ resource "aws_ecs_task_definition" "tf_task" {
         }
       ]
     }
-  ])
+  ]
 
-  volume {
-    name      = "service-storage"
-    host_path = "/ecs/service-storage"
-  }
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-west-2a, us-west-2b]"
-  }
 }
